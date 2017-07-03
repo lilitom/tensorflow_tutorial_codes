36fb65854ac0ca5e5e2cadbce2a3520e51044ed1
minghigh <minghigh2333@gmail.com>
2017-07-03 16:04:06 +0800

    logsticregression on mnist

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 ## Tensorflow系列教程
 
-1. [第一个Tensorflow应用](src/1_first_example/demo.py)
+1. [The first tensorflow model](src/1_first_example/demo.py)
+2. [logistic regression on mnist](src/2_logistic_regression_on_mnist/demo.py)
--- src/1_first_example/demo.py
+++ src/1_first_example/demo.py
@@ -1,38 +1,41 @@
-# 这是tensorflow实现的线性回归例子
+"""
+Simple linear regression example in TensorFlow
+"""
 
 import tensorflow as tf
 
-# 准备数据
+# data preparation
 x_train = [1, 2, 3, 4]
 y_train = [0, -1, -2, -3]
 
-# 定义好模型
+# difine the model
 W = tf.Variable(3., dtype=tf.float32)
 b = tf.Variable(-3., dtype=tf.float32)
 
 x = tf.placeholder(dtype=tf.float32)
-y = tf.placeholder(dtype=tf.float32)
+y_ = tf.placeholder(dtype=tf.float32)
 
-y_ = tf.multiply(x, W) + b  # 我们的线性模型
+y = tf.multiply(x, W) + b  # 我们的线性模型
 
-# 定义loss function
-loss = tf.reduce_mean(tf.square(y - y_))
+# difine the loss function
+loss = tf.reduce_mean(tf.square(y_ - y))
 
-# 创建优化器
+# create optimizer
 optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.01)
 train = optimizer.minimize(loss)
 
-# 迭代模型
+# iterative model
 sess = tf.Session()
 sess.run(tf.global_variables_initializer())
 
-tf.summary.FileWriter(logdir='graphs', graph=sess.graph)
+# tensorboard --logdir "./graphs"
+# tf.summary.FileWriter(logdir='graphs', graph=sess.graph)
 
 for _ in range(1000):
-    sess.run(train, feed_dict={x: x_train, y: y_train})
+    sess.run(train, feed_dict={x: x_train, y_: y_train})
 
-# 查看模型结果
-cur_W, cur_b, cur_loss = sess.run([W, b, loss], feed_dict={x: x_train, y: y_train})
+# check the model results
+cur_W, cur_b, cur_loss = sess.run([W, b, loss], feed_dict={x: x_train, y_: y_train})
 print('W: %s, b: %s, loss: %s' % (cur_W, cur_b, cur_loss))
 
 sess.close()
